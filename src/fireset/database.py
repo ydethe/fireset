1017ba46a1bf4290c1834e15b514db7aee58b9d7
Yann de The <ydethe@gmail.com>
2024-08-08 20:36:12 +0200

    Added postgresql support

--- src/fireset/database.py
+++ src/fireset/database.py
@@ -1,14 +1,28 @@
-import psycopg
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine
+from sqlalchemy import select
 
 from . import settings
 
 
+# https://docs.sqlalchemy.org/en/20/orm/extensions/automap.html
 def list_vcards():
-    # Connect to an existing database
-    with psycopg.connect(settings.database_uri.unicode_string()) as conn:
+    Base = automap_base()
 
-        # Open a cursor to perform database operations
-        with conn.cursor() as cur:
-            # Query the database and obtain data as Python objects.
-            cur.execute("SELECT * FROM organisation")
-            cur.fetchone()
+    engine = create_engine(settings.database_uri.unicode_string())
+
+    # reflect the tables
+    Base.prepare(autoload_with=engine)
+
+    # mapped classes are now created with names by default
+    # matching that of the table name.
+    Contact = Base.classes.contact
+
+    session = Session(engine)
+
+    statement = select(Contact)
+
+    # list of ``User`` objects
+    for user_obj in session.scalars(statement).all():
+        print(user_obj.nom)
---
+++ tests/test_db.py
@@ -0,0 +1,5 @@
+from fireset.database import list_vcards
+
+
+if __name__ == "__main__":
+    list_vcards()
