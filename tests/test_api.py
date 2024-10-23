612ac2beb8c24a85b2507b7ac6bfbeda16c63e5f
Yann de The <ydethe@gmail.com>
2024-10-23 14:11:48 +0200

    Added legacy tests To be adapted

---
+++ tests/test_api.py
@@ -0,0 +1,38 @@
+# Fireset
+# Copyright (C) 2016-2017 Jelmer Vernooĳ <jelmer@jelmer.uk>, et al.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; version 3
+# of the License or (at your option) any later version of
+# the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA  02110-1301, USA.
+
+import shutil
+import tempfile
+import unittest
+
+from fireset.web import FiresetApp, FiresetBackend
+
+
+class WebTests(unittest.TestCase):
+    # When changing this API, please update notes/api-stability.rst and inform
+    # vdirsyncer, who rely on this API.
+
+    def test_backend(self):
+        path = tempfile.mkdtemp()
+        try:
+            backend = FiresetBackend(path)
+            backend.create_principal("foo", create_defaults=True)
+            FiresetApp(backend, "foo")
+        finally:
+            shutil.rmtree(path)
---
+++ tests/test_web.py
@@ -0,0 +1,175 @@
+# Fireset
+# Copyright (C) 2016-2017 Jelmer Vernooĳ <jelmer@jelmer.uk>, et al.
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; version 3
+# of the License or (at your option) any later version of
+# the License.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
+# MA  02110-1301, USA.
+
+"""Tests for fireset.web."""
+
+import os
+import shutil
+import tempfile
+import unittest
+
+from fireset import caldav
+from fireset.icalendar import ICalendarFile
+from fireset.store.git import TreeGitStore
+from fireset.web import CalendarCollection, FiresetBackend
+
+EXAMPLE_VCALENDAR1 = b"""\
+BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:-//bitfire web engineering//DAVdroid 0.8.0 (ical4j 1.0.x)//EN
+BEGIN:VTODO
+CREATED:20150314T223512Z
+DTSTAMP:20150527T221952Z
+LAST-MODIFIED:20150314T223512Z
+STATUS:NEEDS-ACTION
+SUMMARY:do something
+UID:bdc22720-b9e1-42c9-89c2-a85405d8fbff
+END:VTODO
+END:VCALENDAR
+"""
+
+
+class CalendarCollectionTests(unittest.TestCase):
+    def setUp(self):
+        super().setUp()
+        self.tempdir = tempfile.mkdtemp()
+        self.addCleanup(shutil.rmtree, self.tempdir)
+
+        self.store = TreeGitStore.create(os.path.join(self.tempdir, "c"))
+        self.store.load_extra_file_handler(ICalendarFile)
+        self.backend = FiresetBackend(self.tempdir)
+
+        self.cal = CalendarCollection(self.backend, "c", self.store)
+
+    def test_description(self):
+        self.store.set_description("foo")
+        self.assertEqual("foo", self.cal.get_calendar_description())
+
+    def test_color(self):
+        self.assertRaises(KeyError, self.cal.get_calendar_color)
+        self.cal.set_calendar_color("#aabbcc")
+        self.assertEqual("#aabbcc", self.cal.get_calendar_color())
+
+    def test_get_supported_calendar_components(self):
+        self.assertEqual(
+            ["VEVENT", "VTODO", "VJOURNAL", "VFREEBUSY"],
+            self.cal.get_supported_calendar_components(),
+        )
+
+    def test_calendar_query_vtodos(self):
+        def create_fn(cls):
+            f = cls(None)
+            f.filter_subcomponent("VCALENDAR").filter_subcomponent("VTODO")
+            return f
+
+        self.assertEqual([], list(self.cal.calendar_query(create_fn)))
+        self.store.import_one("foo.ics", "text/calendar", [EXAMPLE_VCALENDAR1])
+        result = list(self.cal.calendar_query(create_fn))
+        self.assertEqual(1, len(result))
+        self.assertEqual("foo.ics", result[0][0])
+        self.assertIs(self.store, result[0][1].store)
+        self.assertEqual("foo.ics", result[0][1].name)
+        self.assertEqual("text/calendar", result[0][1].content_type)
+
+    def test_calendar_query_vtodo_by_uid(self):
+        def create_fn(cls):
+            f = cls(None)
+            f.filter_subcomponent("VCALENDAR").filter_subcomponent("VTODO").filter_property(
+                "UID"
+            ).filter_text_match("bdc22720-b9e1-42c9-89c2-a85405d8fbff")
+            return f
+
+        self.assertEqual([], list(self.cal.calendar_query(create_fn)))
+        self.store.import_one("foo.ics", "text/calendar", [EXAMPLE_VCALENDAR1])
+        result = list(self.cal.calendar_query(create_fn))
+        self.assertEqual(1, len(result))
+        self.assertEqual("foo.ics", result[0][0])
+        self.assertIs(self.store, result[0][1].store)
+        self.assertEqual("foo.ics", result[0][1].name)
+        self.assertEqual("text/calendar", result[0][1].content_type)
+
+    def test_get_supported_calendar_data_types(self):
+        self.assertEqual(
+            [("text/calendar", "1.0"), ("text/calendar", "2.0")],
+            self.cal.get_supported_calendar_data_types(),
+        )
+
+    def test_get_max_date_time(self):
+        self.assertEqual("99991231T235959Z", self.cal.get_max_date_time())
+
+    def test_get_min_date_time(self):
+        self.assertEqual("00010101T000000Z", self.cal.get_min_date_time())
+
+    def test_members(self):
+        self.assertEqual([], list(self.cal.members()))
+        self.store.import_one("foo.ics", "text/calendar", [EXAMPLE_VCALENDAR1])
+        result = list(self.cal.members())
+        self.assertEqual(1, len(result))
+        self.assertEqual("foo.ics", result[0][0])
+        self.assertIs(self.store, result[0][1].store)
+        self.assertEqual("foo.ics", result[0][1].name)
+        self.assertEqual("text/calendar", result[0][1].content_type)
+
+    def test_get_member(self):
+        self.assertRaises(KeyError, self.cal.get_member, "foo.ics")
+        self.store.import_one("foo.ics", "text/calendar", [EXAMPLE_VCALENDAR1])
+        result = self.cal.get_member("foo.ics")
+        self.assertIs(self.store, result.store)
+        self.assertEqual("foo.ics", result.name)
+        self.assertEqual("text/calendar", result.content_type)
+
+    def test_delete_member(self):
+        self.assertRaises(KeyError, self.cal.get_member, "foo.ics")
+        self.store.import_one("foo.ics", "text/calendar", [EXAMPLE_VCALENDAR1])
+        self.cal.get_member("foo.ics")
+        self.cal.delete_member("foo.ics")
+        self.assertRaises(KeyError, self.cal.get_member, "foo.ics")
+
+    def test_get_schedule_calendar_transparency(self):
+        self.assertEqual(
+            caldav.TRANSPARENCY_OPAQUE,
+            self.cal.get_schedule_calendar_transparency(),
+        )
+
+    def test_git_refs(self):
+        from ..web import FiresetApp
+        from wsgiref.util import setup_testing_defaults
+
+        self.store.import_one("foo.ics", "text/calendar", [EXAMPLE_VCALENDAR1])
+        app = FiresetApp(self.backend, "user")
+
+        default_branch = self.store.repo.refs.follow(b"HEAD")[0][-1]
+        commit_hash = self.store.repo.refs[default_branch]
+
+        environ = {
+            "PATH_INFO": "/c/.git/info/refs",
+            "REQUEST_METHOD": "GET",
+            "QUERY_STRING": "",
+        }
+        setup_testing_defaults(environ)
+
+        codes = []
+
+        def start_response(code, _headers):
+            codes.append(code)
+
+        body = b"".join(app(environ, start_response))
+
+        self.assertEqual(["200 OK"], codes)
+        self.assertEqual(b"".join([commit_hash, b"\t", default_branch, b"\n"]), body)
